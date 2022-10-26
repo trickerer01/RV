f9fc5eeaa6c2d1ba133f480bf67d6d8b6e0d9ec5
trickerer01 <onlysuffering+1@gmail.com>
2022-10-26 08:22:26 +0700

    rev 61: use list instead of dict for tags consumption (port rules from ruxx), add some authors to allowed tags, increase max length of appended tag to 10

--- tagger.py
+++ tagger.py
@@ -23,15 +23,15 @@ re_replace_symbols = re_compile(
 # used with re.sub
 # noinspection RegExpAnonymousGroup
 re_numbered_or_counted_tag = re_compile(
-    r'^(?!rule_?\d+)(?:\d+?\+?)?([^\d]+?)(?:_\d+|s)?$'
+    r'^(?!rule_?\d+)1?([^\d]+?)(?:_?\d+|s)?$'
 )
 
 re_tags_to_process = re_compile(
     r'^(?:.+?_warc.+?|(?:[a-z]+?_)?elf|drae.{3}|tent[a-z]{3}es|(?:bell[a-z]|sto[a-z]{4})_bul[a-z]{2,3}|inf[a-z]{5}n|egg(?:_[a-z]{3,9}|s)?|'
-    r'[a-z]{4}hral_i.+?|(?:\d{1,2}\+?)?(?:boys?|girls?|fu[a-z]{2}(?:[a-z]{4}|s)?|in[d-v]{2}cts?)|succ[a-z]{4}|'
-    r'bbw|dog|eel|f(?:acesitting|ur)|hmv|pmv|tar|c(?:\.c\.|um)|d\.va|na\'vi|kai\'sa|'
+    r'[a-z]{4}hral_i.+?|(?:\d{1,2}\+?)?(?:boys?|girls?|fu[a-z]{2}(?:[a-z]{4}|s)?|in[d-v]{2}cts?|monsters?)|succ[a-z]{4}|'
+    r'bbw|dog|f(?:acesitting|ur)|hmv|pmv|tar|c(?:\.c\.|um)|monster_girl|'
     r'[^(]+\([^)]+\).*?|[a-z_\-]+\d+?|\d{2,4}[a-z_\-]+?|[a-z_]{2,15}sfm|[^_]+_pov|fu[a-z]{2}(?:/|_(?:on|with)_)[a-z]{4}|'
-    r'[a-z][a-z_]{3,8}|[a-g]ea?st[a-z]{6}|[lapymg]{3})$'
+    r'[a-z][a-z_]{3,9}|[a-g]ea?st[a-z]{6}|[lapymg]{3})$'
 )
 
 re_tags_exclude_major1 = re_compile(
@@ -46,40 +46,41 @@ re_tags_exclude_major2 = re_compile(
 
 re_tags_to_not_exclude = re_compile(
     r'^(?:'
-    r'a(?:liens?|m(?:azonium|b(?:er|rosine))|n(?:al|gel|imopron|thro)|r(?:achnid|iel))|'
-    r'b(?:a(?:ndoned|tesz)|dsm|e(?:a(?:st|r)|n_10|wyx)|i(?:mbo|oshock|r(?:dway|th))|l(?:ack(?:ed)?|o(?:od|wjob))|o(?:dysuit|ndage|vine))|'
-    r'c(?:a(?:ge|nine|t(?:_girl|woman))|entaur|hained|o(?:ot27|rset)|reampie|uckold)|'
-    r'd(?:ark_elf|e(?:a(?:dpool|ath)|er|mons?|ntol|rpixon|zmall)|i(?:ablo|gimon|ldo)|mitrys|o(?:cking|gs?|nkey|om)|ragon(?:ess)?)|'
+    r'a(?:lien(?:_.+?|s)?|m(?:azonium|b(?:er|rosine))|n(?:al|gel|imopron|thro)|r(?:achnid|iel)|yasz)|'
+    r'b(?:a(?:ndoned|ronstrap|tesz)|dsm|e(?:a(?:st|r)|n_10|wyx)|i(?:gjohnson|mbo|oshock|r(?:dway|th))|l(?:ack(?:ed|j.+?)?|o(?:od|wjob))|'
+    r'o(?:dysuit|ndage|vine))|'
+    r'c(?:a(?:ge|nine|t(?:_girl|woman))|entaur|hained|lass_sweeb|o(?:lonelyobo|ot27|rset)|reampie|u(?:ckold|m))|'
+    r'd(?:ark_elf|e(?:a(?:dpool|ath)|er|mons?|ntol|rpixon|zmall)|i(?:ablo|gimon|ldo)|mitrys|o(?:cking|gs?|nkey|om)|ragon(?:ess)?|va)|'
     r'e(?:ggs|lves|nema|quine|xtreme|zria)|'
-    r'f(?:art(?:ing)?|e(?:m(?:boy|dom|shep)|ral)|isting|o(?:rtnite|x_girl)|rozen|u(?:rry|ta(?:holic|nari)))|'
-    r'g(?:a(?:ngbang|p(?:e|ing))|i(?:ant(?:ess)?|fdoozer)|o(?:blins?|o_girl|re|th)|r(?:anny|eatb8)|u(?:il(?:mon|tyk)|robase))|'
-    r'h(?:a(?:iry|l(?:f_elf|o)|ndjob)|e(?:lena|tero)|i(?:gh_elf|nca_p|ve)|mv|o(?:ovesart|r(?:ror|se(?:_sex|girl)?)|usewife)|'
+    r'f(?:a(?:cesitting|rt(?:ing)?)|e(?:m(?:boy|dom|shep)|ral)|isting|o(?:rtnite|x_girl)|rozen|u(?:rry|ta(?:holic|nari)))|'
+    r'g(?:a(?:ngbang|p(?:e|ing))|craw|i(?:ant(?:ess)?|fdoozer)|o(?:blins?|o_girl|re|th)|r(?:an(?:d.+?|ny)|eatb8)|'
+    r'u(?:il(?:mon|tyk)|robase))|'
+    r'h(?:a(?:iry|l(?:f_elf|o)|ndjob)|e(?:lena|tero)|i(?:gh_elf|nca_p|ve)|mv|o(?:ovesart|r(?:ror|se(?:_?(?:cock|girl|s(?:ex)?))?)|usewife)|'
     r'rfidy|ulk|v54rdsl|ydrafxx)|'
-    r'i(?:cedev|demi|n(?:c(?:est|ubus)|justice|sect(?:oid|s)?))|'
+    r'i(?:cedev|demi|n(?:c(?:est|ubus)|justice|sect(?:oid|s)?)|tsmort.+?)|'
     r'j(?:a(?:ckerman|il)|uicyneko)|'
-    r'k(?:a(?:madeva|sdaq|kami)|eycock|hajiit|idnapped|not(?:t(?:ed|ing))?|o(?:opa|rra)|reamu|udoart)|'
-    r'l(?:a(?:osduude|tex)|e(?:ather|eterr|s(?:bian|dias))|o(?:op|punny))|'
+    r'k(?:a(?:isa|m(?:adeva|inak.+?)|sdaq|kami)|eycock|hajiit|idnapped|not(?:t(?:ed|ing))?|o(?:opa|rra)|reamu|udoart)|'
+    r'l(?:a(?:osduude|tex)|e(?:ather|eterr|s(?:bian|dias))|ikkezg|o(?:op|punny))|'
     r'm(?:a(?:chine|g(?:ic|mallow)|id|jora|le(?:_(?:male|only)|sub)?)|ccree|e(?:klab|ltrib|ru|troid)|'
-    r'i(?:dget|driff|ku|lf|n(?:ecraft|otaur|us8)|ruko|s(?:syb|tress))|o(?:nsters?|rty|xxy))|'
-    r'n(?:aga|oih(?:_2)|ualia)|'
+    r'i(?:dget|driff|ku|lf|n(?:ecraft|otaur|us8)|ruko|s(?:syb|tress))|o(?:nster(?:_.+?|s)?|rty|xxy))|'
+    r'n(?:a(?:g(?:a|oonimation)|vi)|o(?:ih(?:_2)|name.+?)|ualia|yl.+?)|'
     r'o(?:gre|ne_piece|p(?:helia|iumud)|r(?:al|cs|gy)|verwatch)|'
     r'p(?:a(?:inful|ladins|ragon|uline)|ersona(?:_\d)?|i(?:kachu|ssing)|mv|o(?:kemon|ny|wergirl)|'
     r'r(?:e(?:dator|gnant)|ison(?:er)?|olapse))|'
-    r'r(?:a(?:d(?:eong3d|roach)|p(?:e|unzel)|tchet)|e(?:becca|dapple2|ey_art)|i(?:eklig|kolo)|u(?:bber|kia)|yona)|'
+    r'r(?:a(?:d(?:eong3d|roachhd)|p(?:e|unzel)|tchet)|e(?:becca|dapple2|ey.+?)|i(?:eklig|kolo)|opeboundart|u(?:bber|kia)|yona)|'
     r's(?:a(?:dako|itou|mira|ntalol|yuri)|ca(?:lie|t)|e(?:cazz?|lf_fuck)|hackles|i(?:lkymilk|ms(?:_\d)?|th_jedi)|k(?:arlet|yrim)|'
-    r'l(?:ave|eepy_b|yxxx24)|mell|o(?:ft_vore|lo(?:_.+?)?|phi[ae]|r(?:aka|idormi))|p(?:i(?:der|troast|zzy)|l(?:atoon|ucky)|o(?:ks|nty))|'
+    r'l(?:ave|eepy_b|yxxx24)|mell|o(?:ft_vore|lo(?:_.+?)?|phi[ae]|r(?:aka|idormi))|p(?:i(?:der|troast|zzy)|l(?:atoon|ucky.+?)|o(?:ks|nty))|'
     r't(?:a(?:lkek|r(?:_.+?|craft|fox))|ra(?:ight|pon)|udio34)|uccubus|ylveon)|'
-    r't(?:a(?:ga|ker_pov)|e(?:kken|ntacles?|xelnaut)|he(?:_sims|count|hoaxxx)|ied|o(?:gruta|rture|uhou)|r(?:a(?:ns|ps?)|inity)|soni|'
-    r'y(?:viania))|'
+    r't(?:a(?:ga|ker_pov)|e(?:k(?:ken|tah.+?)|ntacles?|xelnaut)|he(?:_sims|count|hoaxxx)|ied|o(?:gruta|rture|uhou)|'
+    r'r(?:a(?:ns|ps?)|inity)|soni|y(?:viania))|'
     r'u(?:g(?:ly(?:_man)?|oira)|n(?:birth|de(?:ad|rtale))|r(?:ethral|iel))|'
     r'v(?:a(?:lorant|mpire)|i(?:cer34|olence|rgin)|o(?:mit|re))|'
-    r'w(?:ar(?:craft|frame|hammer)|hip|orld_of_warcraft)|'
+    r'w(?:ar(?:craft|frame|hammer)|eebuv.+?|hip|orld_of_warcraft)|'
     r'x(?:_(?:com(?:_\d)?|ray)|enomorph)|'
     r'z(?:o(?:mbies?|otopia))|'
-    r'\d{1,2}\+?_?(?:animal|boy|futa|girl)s?.+?'  # 0-9
+    r'\d{1,2}\+?_?(?:animal|boy|futa|girl|monster)s?.*?'  # 0-9
     r')$'
 )
-
 
 TAG_ALIASES = {
     'aela_the_huntress_(world_of_warcraft)': 'world_of_warcraft',
@@ -97,13 +98,19 @@ TAG_ALIASES = {
     'illidan_(world_of_warcraft)': 'world_of_warcraft',
     'jaina_proudmoore_(world_of_warcraft)': 'world_of_warcraft',
     'lylly_(world_of_warcraft)': 'world_of_warcraft',
+    'sally_whitemane_(world_of_warcraft)': 'world_of_warcraft',
+    'sylvanas_windrunner_(world_of_warcraft)': 'world_of_warcraft',
     'tess_(world_of_warcraft)': 'world_of_warcraft',
+    'tyrande_whisperwind_(world_of_warcraft)': 'world_of_warcraft',
     'thrall_(world_of_warcraft)': 'world_of_warcraft',
+    'valeera_(world_of_warcraft)': 'world_of_warcraft',
     'vulpera_(world_of_warcraft)': 'world_of_warcraft',
     'worgen_(world_of_warcraft)': 'world_of_warcraft',
     'wrathion_(world_of_warcraft)': 'world_of_warcraft',
     'pmv': 'PMV',
     'hmv': 'HMV',
+    'sfmpmv': 'PMV',
+    'sfmhmv': 'HMV',
 }
 
 
@@ -161,11 +168,10 @@ def filtered_tags(tags_list: List[str]) -> str:
     if len(tags_list) == 0:
         return ''
 
-    tag_chars = '!abcdefghijklmnopqrstuvwxyz'
-    tags_dict = {c: [] for c in tag_chars}  # type: Dict[str, List[str]]
+    tags_list_final = []  # type: List[str]
 
     for tag in tags_list:
-        tag = re_sub(re_replace_symbols, '_', tag)
+        tag = re_sub(re_replace_symbols, '_', tag.replace('-', '').replace('\'', '').replace('.', ''))
         if TAG_ALIASES.get(tag) is None and re_match(re_tags_to_process, tag) is None:
             continue
 
@@ -191,35 +197,37 @@ def filtered_tags(tags_list: List[str]) -> str:
 
         tag = trim_undersores(tag)
 
-        tag_char = tag[0] if tag[0] in tag_chars[1:] else tag_chars[0]
         do_add = True
-        if len(tags_dict[tag_char]) > 0:
+        if len(tags_list_final) > 0:
+            nutag = re_sub(r'[^a-z]+', '', re_sub(re_numbered_or_counted_tag, r'\1', tag))
             # try and see
             # 1) if this tag can be consumed by existing tags
             # 2) if this tag can consume existing tags
-            for i in reversed(range(len(tags_dict[tag_char]))):
-                t = re_sub(re_numbered_or_counted_tag, r'\1', tags_dict[tag_char][i].lower())
-                if len(t) >= len(tag) and (tag in t):
+            for i in reversed(range(len(tags_list_final))):
+                t = re_sub(re_numbered_or_counted_tag, r'\1', tags_list_final[i].lower())
+                nut = re_sub(r'[^a-z]+', '', t)
+                if len(nut) >= len(nutag) and (nutag in nut):
                     do_add = False
                     break
             if do_add:
-                for i in reversed(range(len(tags_dict[tag_char]))):
-                    t = re_sub(re_numbered_or_counted_tag, r'\1', tags_dict[tag_char][i].lower())
-                    if len(tag) >= len(t) and (t in tag):
-                        if aser_valid is False and tags_dict[tag_char][i][0].isupper():
+                for i in reversed(range(len(tags_list_final))):
+                    t = re_sub(re_numbered_or_counted_tag, r'\1', tags_list_final[i].lower())
+                    nut = re_sub(r'[^a-z]+', '', t)
+                    if len(nutag) >= len(nut) and (nut in nutag):
+                        if aser_valid is False and tags_list_final[i][0].isupper():
                             aser_valid = True
-                        del tags_dict[tag_char][i]
+                        del tags_list_final[i]
         if do_add:
             if aser_valid:
                 for i, c in enumerate(tag):  # type: int, str
                     if (i == 0 or tag[i - 1] == '_') and c.isalpha():
                         tag = f'{tag[:i]}{c.upper()}{tag[i + 1:]}'
-            tags_dict[tag_char].append(tag)
+            tags_list_final.append(tag)
 
-    tags_list_final = []
-    [tags_list_final.extend(tag_list) for tag_list in tags_dict.values() if len(tag_list) != 0]
+    # tags_list_final = []
+    # [tags_list_final.extend(tag_list) for tag_list in tags_dict.values() if len(tag_list) != 0]
 
-    return trim_undersores('_'.join(tags_list_final))
+    return trim_undersores('_'.join(sorted(tags_list_final)))
 
 #
 #
