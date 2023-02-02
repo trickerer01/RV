0fa82d168ac93dded059d269ed816dc9fe5aa584
trickerer01 <onlysuffering+1@gmail.com>
2023-02-02 10:36:29 +0700

    rev 159: remove direct ddg bypass acquiring and limit request frequency to 2 per second

--- src/fetch_html.py
+++ src/fetch_html.py
@@ -8,19 +8,15 @@ Author: trickerer (https://github.com/trickerer, https://github.com/trickerer01)
 
 from asyncio import sleep
 from random import uniform as frand
-from re import fullmatch as re_fullmatch
 from typing import Optional
-from urllib import parse
 
 from bs4 import BeautifulSoup
 from aiohttp import ClientSession, ClientResponse, http_parser
 
-from defs import CONNECT_RETRIES_PAGE, Log, DEFAULT_HEADERS, HOST, MAX_VIDEOS_QUEUE_SIZE
+from defs import CONNECT_RETRIES_PAGE, Log, DEFAULT_HEADERS, HOST
 
 proxy = None  # type: Optional[str]
-bypass_in_progress = False
-wellknown_sub = None  # type: Optional[str]
-retries_403 = 0
+request_delay = 0.0
 
 
 class BypassException(Exception):
@@ -33,67 +29,20 @@ def set_proxy(prox: str) -> None:
     proxy = prox
 
 
-async def bypass_ddos_guard_again(s: ClientSession, url: str) -> None:
-    global wellknown_sub
-    wellknown_sub = None
-    await bypass_ddos_guard(s, url)
-
-
-async def bypass_ddos_guard(s: ClientSession, url: str) -> None:
-    global bypass_in_progress
-    global wellknown_sub
-
-    if bypass_in_progress is True:
-        while True:
-            await sleep(1.0)
-            if bypass_in_progress is False:
-                break
-        return
-
-    bypass_in_progress = True
-    try:
-        Log.info('Bypass: fetching bypass cookie...')
-        if wellknown_sub is None:
-            url_parsed = parse.urlparse(url)
-            url_base = f'{url_parsed.scheme}://{url_parsed.netloc}'
-            r = await s.request('GET', 'https://check.ddos-guard.net/check.js', proxy=proxy)
-            if r is None or r.status != 200:
-                Log.error('Bypass: no response from the check host (1)!')
-                raise BypassException(r.status if r is not None else -1)
-            wellknown_sub = re_fullmatch(r'^.*\'(/\.[^\']+)\'.*?$', str(await r.content.read())).group(1)
-            await s.request('GET', f'{url_base}{wellknown_sub}', proxy=proxy)
-            Log.info('Bypass: starting response loop, it will succeed eventually, have patience')
-            while True:
-                r = await s.request('GET', url, proxy=proxy)
-                if r is None or r.status == 403:
-                    Log.info(f'Bypass: response from base host is {r.status if r is not None else -1:d} (2)!')
-                    await sleep(frand(1.0, 7.0))
-                else:
-                    break
-    except BypassException as be:
-        if be.status != 200:
-            assert False
-    except Exception:
-        raise
-    finally:
-        bypass_in_progress = False
-
-
 async def wrap_request(s: ClientSession, method: str, url: str, **kwargs) -> ClientResponse:
-    while bypass_in_progress is True:
-        await sleep(1.0)
+    global request_delay
+    while request_delay > 0.0:
+        d = request_delay
+        request_delay = 0.0
+        await sleep(d)
+    request_delay = 0.5
     s.headers.update(DEFAULT_HEADERS.copy())
     s.cookie_jar.update_cookies({'kt_rt_popAccess': '1', 'kt_tcookie': '1'}, http_parser.URL(HOST))
     kwargs.update(proxy=proxy)
-    r = await s.request(method, url, **kwargs)
-    if r is not None and r.status in [403, 503] and wellknown_sub is None:
-        await bypass_ddos_guard(s, url)
-        r = await s.request(method, url, **kwargs)
-    return r
+    return await s.request(method, url, **kwargs)
 
 
 async def fetch_html(url: str, *, tries: int = None, session: ClientSession) -> Optional[BeautifulSoup]:
-    global retries_403
     # very basic, minimum validation
     tries = tries or CONNECT_RETRIES_PAGE
 
@@ -102,7 +51,6 @@ async def fetch_html(url: str, *, tries: int = None, session: ClientSession) -> 
     retries_403_local = 0
     while retries < tries:
         try:
-            # async with s.request('GET', url, timeout=5, proxy=proxy) as r:
             async with await wrap_request(
                     session, 'GET', url, timeout=5,
                     headers={'X-fancyBox': 'true', 'X-Requested-With': 'XMLHttpRequest', 'Host': HOST, 'Referer': url}) as r:
@@ -124,11 +72,7 @@ async def fetch_html(url: str, *, tries: int = None, session: ClientSession) -> 
             if r is None or r.status != 403:
                 retries += 1
             elif r is not None and r.status == 403:
-                retries_403 += 1
                 retries_403_local += 1
-                if retries_403 >= CONNECT_RETRIES_PAGE * MAX_VIDEOS_QUEUE_SIZE:
-                    await bypass_ddos_guard_again(session, url)
-                    retries_403 = 0
             if retries < tries:
                 await sleep(frand(1.0, 7.0))
             continue
