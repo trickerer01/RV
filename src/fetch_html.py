c558a95bc72d7d91cc4d76919312fb57602bbab2
trickerer01 <onlysuffering+1@gmail.com>
2023-02-11 16:12:20 +0700

    properly implement request queue
    (cherry picked from commit edbacb4ca71646629183ceb993b1195121e97c63)

--- src/fetch_html.py
+++ src/fetch_html.py
@@ -6,9 +6,9 @@ Author: trickerer (https://github.com/trickerer, https://github.com/trickerer01)
 #
 #
 
-from asyncio import sleep
+from asyncio import sleep, get_running_loop, Lock as AsyncLock
 from random import uniform as frand
-from typing import Optional
+from typing import Optional, List
 
 from bs4 import BeautifulSoup
 from aiohttp import ClientSession, ClientResponse, http_parser
@@ -17,16 +17,31 @@ from defs import CONNECT_RETRIES_PAGE, Log, DEFAULT_HEADERS, HOST, CONNECT_REQUE
 
 __all__ = ('wrap_request', 'fetch_html')
 
-request_delay = 0.0
+
+class RequestQueue:
+    _queue = []  # type: List[str]
+    _ready = True
+    _lock = AsyncLock()
+
+    @staticmethod
+    async def _reset() -> None:
+        await sleep(frand(CONNECT_REQUEST_DELAY - 0.25, CONNECT_REQUEST_DELAY + 0.25))
+        RequestQueue._ready = True
+
+    @staticmethod
+    async def until_ready(url: str) -> None:
+        RequestQueue._queue.append(url)
+        while RequestQueue._ready is False or RequestQueue._queue[0] != url:
+            await sleep(0.1)
+        async with RequestQueue._lock:
+            del RequestQueue._queue[0]
+            RequestQueue._ready = False
+            get_running_loop().create_task(RequestQueue._reset())
 
 
 async def wrap_request(s: ClientSession, method: str, url: str, **kwargs) -> ClientResponse:
-    global request_delay
-    while request_delay > 0.0:
-        d = request_delay
-        request_delay = 0.0
-        await sleep(d)
-    request_delay = CONNECT_REQUEST_DELAY
+    await RequestQueue.until_ready(url)
+    Log.debug(f'wrap_request: {url}')
     s.headers.update(DEFAULT_HEADERS.copy())
     s.cookie_jar.update_cookies({'kt_rt_popAccess': '1', 'kt_tcookie': '1'}, http_parser.URL(HOST))
     kwargs.update(proxy=ExtraConfig.proxy)
