bc43c81dde3641e0c1064e99d9eb2ef86a391ed5
trickerer01 <onlysuffering+1@gmail.com>
2023-08-31 21:16:22 +0700

    Deduplicate export code

--- src/export.py
+++ src/export.py
@@ -6,7 +6,7 @@ Author: trickerer (https://github.com/trickerer, https://github.com/trickerer01)
 #
 #
 
-from typing import Dict
+from typing import Dict, Optional, Callable
 
 from defs import Config, normalize_path, prefixp, UTF8
 
@@ -17,50 +17,39 @@ saved_descs_dict = dict()  # type: Dict[str, Dict[int, str]]
 saved_comms_dict = dict()  # type: Dict[str, Dict[int, str]]
 
 
+def register_item_info(sdict: Dict[str, Dict[int, str]], item_id: int, info_str: str, subfolder: str) -> None:
+    if subfolder not in sdict:
+        sdict[subfolder] = dict()
+    sdict[subfolder][item_id] = info_str
+
+
 def register_item_tags(item_id: int, tags_str: str, subfolder: str) -> None:
-    if subfolder not in saved_tags_dict:
-        saved_tags_dict[subfolder] = dict()
-    saved_tags_dict[subfolder][item_id] = tags_str
+    register_item_info(saved_tags_dict, item_id, tags_str, subfolder)
 
 
 def register_item_description(item_id: int, desc_str: str, subfolder: str) -> None:
-    if subfolder not in saved_descs_dict:
-        saved_descs_dict[subfolder] = dict()
-    saved_descs_dict[subfolder][item_id] = desc_str
+    register_item_info(saved_descs_dict, item_id, desc_str, subfolder)
 
 
 def register_item_comments(item_id: int, coms_str: str, subfolder: str) -> None:
-    if subfolder not in saved_comms_dict:
-        saved_comms_dict[subfolder] = dict()
-    saved_comms_dict[subfolder][item_id] = coms_str
+    register_item_info(saved_comms_dict, item_id, coms_str, subfolder)
 
 
 def export_item_info() -> None:
     """Saves tags, descriptions and comments for each subfolder in scenario and base dest folder based on registered item info"""
-    if Config.save_tags is True:
-        for subfolder, tags_dict in saved_tags_dict.items():
-            if len(tags_dict) == 0:
-                continue
-            min_id, max_id = min(tags_dict.keys()), max(tags_dict.keys())
-            fullpath = f'{normalize_path(f"{Config.dest_base}{subfolder}")}{prefixp()}!tags_{min_id:d}-{max_id:d}.txt'
-            with open(fullpath, 'wt', encoding=UTF8) as sfile:
-                sfile.writelines(f'{prefixp()}{idi:d}: {tags.strip()}\n' for idi, tags in sorted(tags_dict.items(), key=lambda t: t[0]))
-    if Config.save_descriptions is True:
-        for subfolder, descs_dict in saved_descs_dict.items():
-            if len(descs_dict) == 0:
-                continue
-            min_id, max_id = min(descs_dict.keys()), max(descs_dict.keys())
-            fullpath = f'{normalize_path(f"{Config.dest_base}{subfolder}")}{prefixp()}!descriptions_{min_id:d}-{max_id:d}.txt'
-            with open(fullpath, 'wt', encoding=UTF8) as sfile:
-                sfile.writelines(f'{prefixp()}{idi:d}:{desc}\n' for idi, desc in sorted(descs_dict.items(), key=lambda t: t[0]))
-    if Config.save_comments is True:
-        for subfolder, comms_dict in saved_comms_dict.items():
-            if len(comms_dict) == 0:
-                continue
-            min_id, max_id = min(comms_dict.keys()), max(comms_dict.keys())
-            fullpath = f'{normalize_path(f"{Config.dest_base}{subfolder}")}{prefixp()}!comments_{min_id:d}-{max_id:d}.txt'
-            with open(fullpath, 'wt', encoding=UTF8) as sfile:
-                sfile.writelines(f'{prefixp()}{idi:d}:{coms}\n' for idi, coms in sorted(comms_dict.items(), key=lambda t: t[0]))
+    for conf, sdict, name, proc_cb in (
+        (Config.save_tags, saved_tags_dict, 'tags', lambda tags: f' {tags.strip()}\n'),
+        (Config.save_descriptions, saved_descs_dict, 'descriptions', lambda description: f'{description}\n'),
+        (Config.save_comments, saved_comms_dict, 'comments', lambda comment: f'{comment}\n')
+    ):  # type: Optional[bool], Dict[str, Dict[int, str]], str, Callable[[str], str]
+        if conf is True:
+            for subfolder, subdict in sdict.items():
+                if len(subdict) == 0:
+                    continue
+                min_id, max_id = min(subdict.keys()), max(subdict.keys())
+                fullpath = f'{normalize_path(f"{Config.dest_base}{subfolder}")}{prefixp()}!{name}_{min_id:d}-{max_id:d}.txt'
+                with open(fullpath, 'wt', encoding=UTF8) as sfile:
+                    sfile.writelines(f'{prefixp()}{idi:d}:{proc_cb(elem)}' for idi, elem in sorted(subdict.items(), key=lambda t: t[0]))
 
 #
 #
