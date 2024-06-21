bd1f12df19facb3b65b3f07dca41a2a97eb8a7aa
trickerer01 <onlysuffering+1@gmail.com>
2024-06-21 20:30:36 +0700

    Fix missing new default-unlisted tag

--- src/version.py
+++ src/version.py
@@ -9,7 +9,7 @@ Author: trickerer (https://github.com/trickerer, https://github.com/trickerer01)
 APP_NAME = 'RV'
 APP_VER_MAJOR = '1'
 APP_VER_SUB = '8'
-APP_REVISION = '462'
+APP_REVISION = '463'
 APP_VERSION = f'{APP_VER_MAJOR}.{APP_VER_SUB}.{APP_REVISION}'
 
 #
