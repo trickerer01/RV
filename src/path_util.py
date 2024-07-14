4fd5a0644629424e267c7c3d13c0bec220a15664
trickerer01 <onlysuffering+1@gmail.com>
2024-07-14 18:42:31 +0700

    Improve pre-filter performance for large queues

--- src/path_util.py
+++ src/path_util.py
@@ -7,7 +7,7 @@ Author: trickerer (https://github.com/trickerer, https://github.com/trickerer01)
 #
 
 from os import path, listdir, rename, makedirs, getpid
-from typing import List, Dict, MutableSequence
+from typing import List, Dict, Tuple, MutableSequence
 
 from psutil import Error as PSError, process_iter
 
@@ -21,6 +21,7 @@ from vinfo import VideoInfo
 __all__ = ('file_already_exists', 'file_already_exists_arr', 'try_rename', 'prefilter_existing_items', 'is_file_being_used')
 
 found_filenames_dict: Dict[str, List[str]] = dict()
+media_matches_cache: Dict[str, Tuple[str, Quality]] = dict()
 
 
 def report_duplicates() -> None:
@@ -99,42 +100,40 @@ def scan_dest_folder() -> None:
         report_duplicates()
 
 
-def file_exists_in_folder(base_folder: str, idi: int, quality: Quality) -> str:
-    orig_file_names = found_filenames_dict.get(normalize_path(base_folder))
-    if path.isdir(base_folder) and orig_file_names is not None:
+def get_media_file_match(fname: str) -> Tuple[str, Quality]:
+    if fname not in media_matches_cache:
+        f_match = re_media_filename.match(fname)
+        f_id, f_quality = (f_match.group(1), Quality(f_match.group(2) or '')) if f_match else ('', '')
+        media_matches_cache[fname] = (f_id, f_quality)
+    return media_matches_cache[fname]
+
+
+def file_exists_in_folder(base_folder: str, idi: int, quality: Quality, check_folder: bool) -> str:
+    orig_file_names = found_filenames_dict.get(base_folder)
+    if (not check_folder or path.isdir(base_folder)) and orig_file_names is not None:
         for fname in orig_file_names:
-            try:
-                f_match = re_media_filename.match(fname)
-                f_id = f_match.group(1)
-                f_quality = Quality(f_match.group(2) or '')
-                if str(idi) == f_id and (not quality or not f_quality or quality <= f_quality):
-                    return f'{normalize_path(base_folder)}{fname}'
-            except Exception:
-                continue
+            f_id, f_quality = get_media_file_match(fname)
+            if f_id and str(idi) == f_id and (not quality or not f_quality or quality <= f_quality):
+                return f'{normalize_path(base_folder)}{fname}'
     return ''
 
 
-def file_already_exists(idi: int, quality: Quality) -> str:
+def file_already_exists(idi: int, quality: Quality, check_folder=True) -> str:
     for fullpath in found_filenames_dict:
-        fullpath = file_exists_in_folder(fullpath, idi, quality or Config.quality)
+        fullpath = file_exists_in_folder(fullpath, idi, quality or Config.quality, check_folder)
         if len(fullpath) > 0:
             return fullpath
     return ''
 
 
 def file_exists_in_folder_arr(base_folder: str, idi: int, quality: Quality) -> List[str]:
-    orig_file_names = found_filenames_dict.get(normalize_path(base_folder))
+    orig_file_names = found_filenames_dict.get(base_folder)
     folder_files = list()
     if path.isdir(base_folder) and orig_file_names is not None:
         for fname in orig_file_names:
-            try:
-                f_match = re_media_filename.match(fname)
-                f_id = f_match.group(1)
-                f_quality = Quality(f_match.group(2) or '')
-                if str(idi) == f_id and (not quality or not f_quality or quality == f_quality):
-                    folder_files.append(f'{normalize_path(base_folder)}{fname}')
-            except Exception:
-                continue
+            f_id, f_quality = get_media_file_match(fname)
+            if f_id and str(idi) == f_id and (not quality or not f_quality or quality == f_quality):
+                folder_files.append(f'{normalize_path(base_folder)}{fname}')
     return folder_files
 
 
@@ -157,7 +156,7 @@ def prefilter_existing_items(vi_list: MutableSequence[VideoInfo]) -> None:
 
     i: int
     for i in reversed(range(len(vi_list))):
-        fullpath = file_already_exists(vi_list[i].id, Quality())
+        fullpath = file_already_exists(vi_list[i].id, Quality(), False)
         if len(fullpath) > 0:
             Log.info(f'Info: {vi_list[i].sname} found in \'{path.split(fullpath)[0]}/\'. Skipped.')
             del vi_list[i]
