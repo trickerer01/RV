55778db623da975f12f85b3c1c08a34abf4b6006
trickerer01 <onlysuffering+1@gmail.com>
2025-12-08 22:23:49 +0700

    Fix package path extension appending

--- rv/__main__.py
+++ rv/__main__.py
@@ -1,6 +1,6 @@
 import sys
 
-if __package__ is None and not getattr(sys, 'frozen', False):
+if not __package__ and not getattr(sys, 'frozen', False):
     import os.path
     path = os.path.realpath(os.path.abspath(__file__))
     sys.path.insert(0, os.path.dirname(os.path.dirname(path)))
